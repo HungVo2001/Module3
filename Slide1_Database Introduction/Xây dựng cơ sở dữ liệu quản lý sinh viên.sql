9b9eed7750bb6fe321a3c49fcf8f82130efd344f
HungVo2001 <naclub55@gmail.com>
2023-06-22 17:13:12 +0700

    add file

---
+++ Slide1_Database Introduction/Xây dựng cơ sở dữ liệu quản lý sinh viên.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `student-management`.`class` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+CREATE TABLE `student-management`.`teacher` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `age` INT NULL,
+  `country` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
